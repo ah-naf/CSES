371d8f50163b90642301568c3372a5ff1bc2f9e4
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-04-21 15:53:37 +0600

    Create Two Sets.cpp

---
+++ Two Sets.cpp
@@ -0,0 +1,69 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     100007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)               for(auto it: a) cout<<it<<" "
+ 
+int main()
+{
+    AHNAF;
+    int ca=0;
+    ll n;
+    cin>>n;
+    ll sum=(n*(n+1))/2;
+    if(sum%2==0)
+    {
+        //YES;
+        ll cnt=sum/2;
+        int c[MAX]= {0};
+        vii v,vv;
+        for(int i=n; i>0; i--)
+        {
+            if(cnt-i>=0)
+            {
+                v.pb(i);
+                cnt-=i;
+            }
+            else {
+                vv.pb(i);
+            }
+        }
+            YES;
+            cout<<v.size()<<endl;
+            print(v);cout<<endl;
+            cout<<vv.size()<<endl;
+            print(vv);cout<<endl;
+    }
+    else NO;
