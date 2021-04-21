aadd2514b8e8c2734137a8c7f31acccb4ab8d05a
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-04-21 15:51:14 +0600

    Create Increasing Array.cpp

---
+++ Increasing Array.cpp
@@ -0,0 +1,55 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+ 
+int main()
+{
+    AHNAF;
+    int n;cin>>n;
+    vll v(n);
+    for(int i=0; i<n; i++) cin>>v[i];
+    ll sum=0;
+    for(int i=0; i<n-1; i++)
+    {
+        if(v[i]>v[i+1])
+        {
+            sum+=abs(v[i]-v[i+1]);
+            v[i+1]=v[i];
+        }
+ 
+    }
+    cout<<sum;
+}
